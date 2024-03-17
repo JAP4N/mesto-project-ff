f9b0e0a8a70799fff4e0efbdf13bcfb6ea6d0bbf
Maksim <malk0v.mak@yandex.ru>
2024-03-17 12:10:54 +0300

    finised

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# Проектная работа Mesto
+# ✨Проект "Mesto" #
+
+## Используемые технологии ##
+- HTML
+- CSS
+- JavaScript
+
+## 👉Ссылка на проект ##
+[https://github.com/JAP4N/mesto-project-ff.git](https://github.com/JAP4N/mesto-project-ff.git)
 
--- scripts/index.js
+++ scripts/index.js
@@ -2,8 +2,34 @@
 
 // @todo: DOM узлы
 
+import { initialCards }  from './cards.js';
+
+const cardList = document.querySelector(".places__list");
+
 // @todo: Функция создания карточки
+
+function creatCard (cardValue, deleteCard) {
+    const cardTemplate = document.querySelector("#card-template").content;
+    const cardItem = cardTemplate.querySelector(".card").cloneNode(true);
+
+    const cardTitle = cardItem.querySelector(".card__title").textContent = cardValue.name;
+    const cardImage = cardItem.querySelector(".card__image").setAttribute("src", cardValue.link);
+
+    cardList.append(cardItem);
+
+    cardItem.querySelector(".card__delete-button").addEventListener("click", deleteCallBack);
+
+    return cardItem;
+}
 
 // @todo: Функция удаления карточки
 
+function deleteCallBack(evt) {
+    let card = evt.target.closest(".card");
+    card.remove();
+}
+
+
 // @todo: Вывести карточки на страницу
+
+initialCards.forEach(creatCard);
