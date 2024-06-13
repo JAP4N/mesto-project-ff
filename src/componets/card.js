e751d204412e63098517f3195064bf501b348d6d
Maks <malk0v.mak@yandex.ru>
2024-06-13 23:32:58 +0300

    didnt_repair_close-btn

--- src/componets/card.js
+++ src/componets/card.js
@@ -1,27 +1,29 @@
-import { popupNewCard, cardTemplate, cardList , deleteCallBack} from "../scripts/index.js"
+import { popupNewCard, cardTemplate, cardList, deleteCallBack, createCard} from "../scripts/index.js"
 
 export const handleCreateCard = evt => {
     evt.preventDefault();
 
-    const newHandleCard = cardTemplate.querySelector(".card").cloneNode(true);
+    const nameCardPopup = popupNewCard.querySelector(".popup__input_type_card-name").value;
+    const linkImgPopup = popupNewCard.querySelector(".popup__input_type_url").value;
 
-    const nameCard = popupNewCard.querySelector(".popup__input_type_card-name");
-    const urlImg = popupNewCard.querySelector(".popup__input_type_url");
-    const nameCardValue = nameCard.value;
-    const urlImgValue = urlImg.value;
-    newHandleCard.querySelector(".card__delete-button").addEventListener("click", deleteCardBtn);
-    newHandleCard.querySelector(".card__like-button").addEventListener("click", likeCardBtn);
+    const newCardfromPopup = createCard ({nameCardPopup, linkImgPopup}, deleteCardBtn)
+    // const nameCard = popupNewCard.querySelector(".popup__input_type_card-name");
+    // const urlImg = popupNewCard.querySelector(".popup__input_type_url");
+    // const nameCardValue = nameCard.value;
+    // const urlImgValue = urlImg.value;
+    // cardItem.querySelector(".card__title").textContent = nameCardValue;
+    // cardItem.querySelector(".card__image").setAttribute("src", urlImgValue);
+    // cardItem.querySelector(".card__delete-button").addEventListener("click", deleteCardBtn);
+    // cardItem.querySelector(".card__like-button").addEventListener("click", likeCardBtn);
 
-    newHandleCard.querySelector(".card__title").textContent = nameCardValue;
-    newHandleCard.querySelector(".card__image").setAttribute("src", urlImgValue);
     // Очищаем поля формы
-    nameCard.value = "";
-    urlImg.value = "";
+    // nameCard.value = "";
+    // urlImg.value = "";
     
     // Закрываем форму
     popupNewCard.classList.remove("popup_is-opened");
     
-    cardList.prepend(newHandleCard);
+    cardList.prepend(newCardfromPopup);
 };
 
 //удаление карточки
@@ -30,7 +32,7 @@ export const deleteCardBtn = evt => {
 };
 
 
-//функция добавления like'а
+// функция добавления like'а
 export const likeCardBtn = evt => {
     if (evt.target.classList.contains("card__like-button")) {
         console.log(evt.target);
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,9 +1,9 @@
 import '/src/pages/index.css';
 
 //import
-import { initialCards }  from './cards.js';
-import { openPopup, closePopup, handleFormSubmit }  from '../componets/modal.js'
-import { handleCreateCard, likeCardBtn, deleteCardBtn }  from '../componets/card.js'
+import { initialCards } from './cards.js';
+import { openPopup, closePopup, handleFormSubmit } from '../componets/modal.js'
+import { handleCreateCard, likeCardBtn } from '../componets/card.js'
 
 //export
 export { popupList, popupCloseBtnAll, profileEditButton, popupEdit, popupNewCard, cardList, cardTemplate }
@@ -30,17 +30,23 @@ const jobInput = popupEdit.querySelector(".popup__input_type_description");
 
 //Темплейт карточки
 const cardTemplate = document.querySelector("#card-template").content;
-
 const cardList = document.querySelector(".places__list");
 
 //Функция создания карточки
-function createCard (cardValue, deleteCard, likeCardBtn) {
+export function createCard ({name, link}, deleteCard) {
     const cardItem = cardTemplate.querySelector(".card").cloneNode(true);
-    cardItem.addEventListener("click", likeCardBtn);
-    const cardTitle = cardItem.querySelector(".card__title").textContent = cardValue.name;
-    const cardImage = cardItem.querySelector(".card__image").setAttribute("src", cardValue.link);
+    const cardTitle = cardItem.querySelector(".card__title").textContent = name;
+    const cardImage = cardItem.querySelector(".card__image").src = link;                                //setAttribute("src", cardValue.link);
+
+    // const nameCardPopup = popupNewCard.querySelector(".popup__input_type_card-name").value;
+    // const linkImgPopup = popupNewCard.querySelector(".popup__input_type_url").value;
+    // const nameCardValue = nameCardPopup.value;
+    // const urlImgValue = linkImgPopup.value;
+    // cardItem.querySelector(".card__title").textContent = nameCardValue;
+    // cardItem.querySelector(".card__image").setAttribute("src", urlImgValue);
 
     cardItem.querySelector(".card__delete-button").addEventListener("click", deleteCard);
+    cardItem.querySelector(".card__like-button").addEventListener("click", likeCardBtn);
 
     return cardItem;
 }
@@ -52,7 +58,7 @@ export function deleteCallBack(evt) {
 
 //Вывести карточки на страницу
 initialCards.forEach(cardData => {
-    const newCardItem = createCard(cardData, deleteCallBack, likeCardBtn);
+    const newCardItem = createCard(cardData, deleteCallBack);
     cardList.append(newCardItem);
 });
 
